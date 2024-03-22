3e8d658b805aef80226eda05ef01ffe442d43de9
blue-ssom <somi5404@naver.com>
2024-03-22 15:22:35 +0000

    implement the code for comment like API

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -190,7 +190,63 @@ router.delete('/:postIdx/:commentIdx', async (req, res) => {
     }
 
 });
-// 댓글 좋아요
+
+// 댓글 좋아요 추가
+router.post('/:commentIdx/like', async (req, res) => {
+    const commentIdx = req.params.commentIdx; // 댓글 ID
+    const sessionUserIdx = req.session.userIdx; // 세션에 저장된 사용자 ID
+    console.log("댓글 좋아요 세션: ", sessionUserIdx)
+
+
+    const result = {
+        "success": false,
+        "message": "",
+        "data": null
+    };
+
+    try {
+
+        // 세션에 사용자 ID가 없는 경우
+        // if (!sessionUserIdx) {
+        //     throw new Error("잘못된 접근입니다.");
+        // }
+        
+         // 좋아요 여부 확인
+        const likeCheckSQL = `
+            SELECT * FROM scheduler.comment_likes
+            WHERE comment_idx = $1 AND user_idx = $2;
+        `;
+        const likeCheckResult = await pool.query(likeCheckSQL, [commentIdx, sessionUserIdx]);
+
+        if (likeCheckResult.rows.length > 0) {
+            throw new Error("이미 댓글에 좋아요를 누르셨습니다.");
+        }
+
+        // DB통신: 댓글 좋아요 추가
+        const likeInsertSQL = `
+            INSERT INTO scheduler.comment_likes (comment_idx, user_idx)
+            VALUES ($1, $2);
+        `;
+        await pool.query(likeInsertSQL, [commentIdx, sessionUserIdx]);
+
+        // DB통신: 댓글의 좋아요 수 증가
+        const updateLikesCountSQL = `
+            UPDATE scheduler.comment
+            SET likes_count = likes_count + 1
+            WHERE comment_idx = $1;
+        `;
+        await pool.query(updateLikesCountSQL, [commentIdx]);
+
+        // 결과 설정
+        result.success = true;
+        result.message = "댓글 좋아요 추가 성공";
+    } catch (e) {
+        result.message = e.message;
+    } finally {
+        res.send(result);
+    }
+});
+
 // 댓글 좋아요 취소
 
 // export 작업
